b33809a7276658c086d1450132f6ffcbdb767772
Jonah Pierce <fellowhashbrown@gmail.com>
2020-10-31 16:38:47 +0000

    Implemented Quotes database methods

---
+++ api_methods/quotes/quotes.py
@@ -0,0 +1,20 @@
+from api_methods.base_api import BaseAPI
+
+class QuotesAPI:
+    """The QuotesAPI class handles the Llamas quotes and 
+    The Office quotes
+    """
+
+    class Llamas(BaseAPI):
+        """
+        """
+
+        def get(self):
+            pass
+    
+    class TheOffice(BaseAPI):
+        """
+        """
+
+        def get(self):
+            pass
--- database/db/quotes.py
+++ database/db/quotes.py
@@ -1,32 +1,107 @@
 from os import environ
+from random import randint, choice
+
 from pymongo import MongoClient
 
+
 class DBQuotes:
+    """This class contains methods to modify the Quotes database"""
 
     CLIENT = MongoClient(f"mongodb+srv://{environ['DATABASE_USERNAME']}:{environ['DATABASE_PASSWORD']}@fellowhashbrown.orymo.gcp.mongodb.net/quotes?retryWrites=true&w=majority")
 
     def __init__(self):
-        self.__theOffice = DBQuotes.CLIENT.theOffice
-        self.__llamasWithHats = DBQuotes.CLIENT.llamasWithHats
-        self.__brooklyn99 = DBQuotes.CLIENT.brooklyn99
-        self.__parksAndRec = DBQuotes.CLIENT.parksAndRec
+        self.__the_office = DBQuotes.CLIENT.quotes["theOffice"]
+        self.__llamas_with_hats = DBQuotes.CLIENT.quotes["llamasWithHats"]
+        self.__brooklyn_99 = DBQuotes.CLIENT.quotes["brooklyn99"]
+        self.__parks_and_rec = DBQuotes.CLIENT.quotes["parksAndRec"]
     
     # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
-    def get_office_quotes(self):
-        pass
+    @property
+    def the_office(self):
+        return self.__the_office
     
-    def get_office_quote(self, season: int=None, episode: int=None, author:str=None, *, random: bool=True):
-        pass
+    @property
+    def llamas_with_hats(self):
+        return self.__llamas_with_hats
+    
+    @property
+    def brooklyn_99(self):
+        return self.__brooklyn_99
+    
+    @property
+    def parks_and_rec(self):
+        return self.__parks_and_rec
+
+    # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+    def get_office_quotes(self):
+        """Returns all the office quotes in the database"""
+        return {
+            doc["_id"]: doc["episodes"]
+            for doc in self.the_office.find()
+        }
+    
+    def get_office_quote(self, season: int=None, episode: int=None, author: str=None, is_deleted=False):
+        """Returns an office quote from a specific season, episode, and author"""
+        quotes = self.get_office_quotes()
+
+        # Get a random season and episode
+        if season is None and episode is None:
+            season = randint(1, len(quotes)) + 1
+            episode = randint(1, len(quotes[f"season{season}"]))
+        elif episode is None:
+            if not(1 <= season <= 9):
+                return { "error": "Invalid season" }
+            episode = randint(1, len(quotes[f"season{season}"]))
+        elif season is None:
+            return { "error": "Cannot get random episode from unspecified season" }
+        
+        # Validate the season and episode
+        if not(1 <= season <= 9):
+            return { "error": "Invalid season" }
+        if not(1 <= episode <= len(quotes[f"season{season}"])):
+            return {" error": "Invalid episode" }
+        quotes = quotes[f"season{season}"][episode - 1]
+        
+        # Search through the quotes given a specific filter of author and is_deleted
+        filtered_quotes = []
+        for quote in quotes:
+            if ((author is None or 
+                    quote["author"].lower() == author.lower()) and
+                    (is_deleted is None or
+                    is_deleted == quote["deleted"])):
+                filtered_quotes.append(quote)
+        return choice(filtered_quotes)
     
     # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
     def get_llamas_quotes(self):
-        pass
+        """Returns all the llamas with hates quotes in the database"""
+        return {
+            doc["_id"]: doc["quotes"]
+            for doc in self.llamas_with_hats.find()
+        }
     
-    def get_llamas_quote(self, episode: int=None, author: str=None, *, random: bool=True):
-        pass
-    
+    def get_llamas_quote(self, episode: int=None, author: str=None):
+        """Returns a llamas with hats quote from a specific episode and author"""
+        quotes = self.get_llamas_quotes()
+
+        # Get a random episode
+        #   or validate the episode
+        if episode is None:
+            episode = randint(1, len(quotes))
+        if not(1 <= episode <= len(quotes)):
+            return { "error": "Invalid episode" }
+        quotes = quotes[f"episode{episode}"]["quotes"]
+        
+        # Search through the quotes given a specific filter of author
+        filtered_quotes = []
+        for quote in quotes:
+            if author is None or quote["author"].lower() == author.lower:
+                filtered_quotes.append(quote)
+        return choice(filtered_quotes)
+
     # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
     def get_brooklyn_99_quotes(self):
